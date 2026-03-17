8961e837b7782e39ff8ae035d504e9dc47b6ee06
Jsaur <6awesomejoe@gmail.com>
2026-03-17 12:51:48 -0400

    fix #14, implement EvaluationResult.java

--- src/main/java/com/umlcc/model/EvaluationResult.java
+++ src/main/java/com/umlcc/model/EvaluationResult.java
@@ -1,20 +1,41 @@
 package com.umlcc.model;
 
+/**
+ * The result of a compliance evaluation.
+ * @author Joe Hardy
+ */
 public class EvaluationResult {
     private Warning warning;
     private JavaThing thisThing;
     private JavaThing otherThing;
-    private String message;
+    private Config config;
 
+    /**
+     * Constructor for an EvaluationResult that does not involve comparison.
+     * @param warning the warning of this EvaluationResult.
+     * @param thisThing the JavaThing this EvaluationResult goes with.
+     */
     public EvaluationResult(Warning warning, JavaThing thisThing) {
-
+        this.warning = warning;
+        this.thisThing = thisThing;
+        config = Config.getInstance();
     }
 
+    /**
+     * Constructor for an EvaluationResult that involves comparison.
+     * @param warning the warning of this EvaluationResult.
+     * @param thisThing the JavaThing this EvaluationResult goes with.
+     * @param otherThing the JavaThing that thisThing is being compared to.
+     */
     public EvaluationResult(Warning warning, JavaThing thisThing, JavaThing otherThing) {
-
+        this.warning = warning;
+        this.thisThing = thisThing;
+        this.otherThing = otherThing;
+        config = Config.getInstance();
     }
 
-    public String getMessage() {
-        return null;
+    @Override
+    public String toString() {
+        return config.getWarningMessage(warning) + " | " + thisThing.getName();
     }
 }
